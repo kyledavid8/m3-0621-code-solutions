553b733d27434f216c5baba88c201be31689f4c2
Kyle Cunningham <kyle8david@gmail.com>
2021-07-13 21:01:42 -0600

    completed react-accordion-component

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -6,9 +6,7 @@ class Accordion extends React.Component {
     super(props);
     this.props = props;
     this.state = {
-      "one": "hidden",
-      "two": "hidden",
-      "three": "hidden"
+      openId: null
     }
     this.handleOpen = this.handleOpen.bind(this);
     this.handleClose = this.handleClose.bind(this);
@@ -20,40 +18,28 @@ class Accordion extends React.Component {
 
     return (
       <div className="container" onDoubleClick={this.handleClose}>
-        <div className="one" onClick={this.handleOpen}>
-          {prop[0].name}
-        </div>
-        <div className={`text one ${state.one}`}>{prop[0].description}</div>
-        <div className="two" onClick={this.handleOpen}>
-          {prop[1].name}
-        </div>
-        <div className={`text two ${state.two}`}>{prop[1].description}</div>
-        <div className="three" onClick={this.handleOpen}>
-          {prop[2].name}
-        </div>
-        <div className={`text three ${state.three}`}>{prop[2].description}</div>
+        {
+          prop.map(item => {
+            return (
+              <div key={item.id}>
+                <div className="accordion-item" onClick={() => this.handleOpen(item.id)}>
+                  {item.name}
+                </div>
+                <div className={`accordion-description ${state.openId === item.id ? 'on' : 'hidden'}`}>{item.description}</div>
+              </div>
+            )
+          })
+        }
       </div>
     )
   }
 
-  handleOpen(event) {
-    let target = event.target.className;
-    this.setState({
-      "one": "hidden",
-      "two": "hidden",
-      "three": "hidden"
-    })
-    this.setState({
-      [target]: "on"
-    })
+  handleOpen(id) {
+    this.setState({openId: id})
   }
 
   handleClose(event) {
-    this.setState({
-      "one": "hidden",
-      "two": "hidden",
-      "three": "hidden"
-    })
+    this.setState({openId: null})
   }
 }
 
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -5,9 +5,9 @@ import Accordion from './accordion';
 ReactDOM.render(
   <Accordion content={
     [
-      { name: "Hypertext Markup Language", description: "Hypertext Markup Language, a standardized system for tagging text files to achieve font, color, graphic, and hyperlink effects on World Wide Web pages." },
-      { name: "Cascading Style Sheets", description: "Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML." },
-      { name: "Javascript", description: "An object-oriented computer programming language commonly used to create interactive effects within web browsers."}
+      { id: 1, name: "Hypertext Markup Language", description: "Hypertext Markup Language, a standardized system for tagging text files to achieve font, color, graphic, and hyperlink effects on World Wide Web pages." },
+      { id: 2, name: "Cascading Style Sheets", description: "Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML." },
+      { id: 3, name: "Javascript", description: "An object-oriented computer programming language commonly used to create interactive effects within web browsers."}
     ]
   }/>,
   document.querySelector('#root')
